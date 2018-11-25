7db1d120396de74d61f1ed9b9ec13aacfdb7ae80
Andres Aguilar <andresyoshimar@gmail.com>
2018-11-24 20:47:44 -0600

    Agregando proyecto con vue + vuetify

---
+++ personal/src/components/MyToolbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <v-toolbar class="white">
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn flat href="#" target="_blank"> <span class="mr-2">Login</span> </v-btn>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyToolbar',
+    props: ['title'],
+}
+</script>
